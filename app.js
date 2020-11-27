48582036eb2739ba8153d7e4f463c5f4001423fa
Vedant Shinde <vedantshinde101@gmail.com>
2020-11-27 18:31:35 +0530

    Implement CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -25,6 +26,13 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1)GLOBAL MIDDLEWARES
+
+// Implement CORS
+app.use(cors());
+
+// For pre-flight browser requests(for non-simple requests) like PATCH & DELETE
+app.options('*', cors());
+
 //serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
